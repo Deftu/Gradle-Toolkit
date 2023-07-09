41e43d59c3322a6c6c70c909749c2faf59add2f4
Deftu <deftudev@gmail.com>
2023-07-10 00:19:32 +0200

    Update repo.gradle.kts

--- src/main/kotlin/xyz/deftu/gradle/tools/repo.gradle.kts
+++ src/main/kotlin/xyz/deftu/gradle/tools/repo.gradle.kts
@@ -7,19 +7,16 @@ repositories {
     mavenCentral()
     mavenLocal()
 
+    // General
     optionalMaven("repo.deftu.releases", "Deftu Releases", "https://maven.deftu.xyz/releases/")
     optionalMaven("repo.jitpack", "JitPack", "https://jitpack.io/")
     optionalMaven("repo.deftu.snapshots", "Deftu Snapshots", "https://maven.deftu.xyz/snapshots/")
-}
 
-afterEvaluate {
-    repositories {
-        if (isLoomPresent()) {
-            optionalMaven("repo.essential", "Essential", "https://repo.essential.gg/repository/maven-public/")
-            optionalMaven("repo.sponge", "SpongePowered", "https://repo.spongepowered.org/maven/")
-            optionalMaven("repo.terraformers", "Terraformers", "https://maven.terraformersmc.com/releases/")
-        }
-    }
+    // Minecraft
+    optionalMaven("repo.essential", "Essential", "https://repo.essential.gg/repository/maven-public/")
+    optionalMaven("repo.sponge", "SpongePowered", "https://repo.spongepowered.org/maven/")
+    optionalMaven("repo.kff", "KotlinForForge", "https://thedarkcolour.github.io/KotlinForForge/")
+    optionalMaven("repo.terraformers", "Terraformers", "https://maven.terraformersmc.com/releases/")
 }
 
 fun RepositoryHandler.optionalMaven(propertyName: String, name: String? = null, url: String) {
