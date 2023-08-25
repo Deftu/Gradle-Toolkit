5dcdf15f63c93c495f09acfebcfacc4ea86d4d71
Deftu <deftudev@gmail.com>
2023-08-25 14:05:23 +0200

    Update Kotlin, Preprocessor, Minotaur and Blossom

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version("1.6.21")
+    kotlin("jvm") version("1.9.0")
     `kotlin-dsl`
     val dgt = "1.18.7+"
     id("xyz.deftu.gradle.tools.repo") version(dgt)
@@ -42,15 +42,15 @@ dependencies {
     implementation("dev.architectury:architectury-pack200:0.1.3")
 
     // Preprocessing/multi-versioning
-    implementation("xyz.deftu:preprocessor:0.1.0")
+    implementation("xyz.deftu:preprocessor:0.3.0")
 
     // Other
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.21")
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
-    implementation("net.kyori:blossom:1.3.0")
+    implementation("net.kyori:blossom:1.3.1")
 
     // Publishing
-    implementation("com.modrinth.minotaur:Minotaur:2.7.2")
+    implementation("com.modrinth.minotaur:Minotaur:2.8.2")
     implementation("net.darkhax.curseforgegradle:CurseForgeGradle:1.0.11")
     implementation("com.github.breadmoirai:github-release:2.4.1")
 }
