7e513a5fb06b9141ed56ecfbde09c1cd9fa4b07a
Deftu <deftudev@gmail.com>
2024-06-07 12:03:49 +0200

    No more deprecated kotlinOptions

--- src/main/kotlin/dev/deftu/gradle/tools/kotlin.gradle.kts
+++ src/main/kotlin/dev/deftu/gradle/tools/kotlin.gradle.kts
@@ -3,13 +3,14 @@ package dev.deftu.gradle.tools
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import dev.deftu.gradle.utils.getJavaVersionAsInt
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 val version = getJavaVersionAsInt()
 if (version != 0) {
     val javaVersion = JavaVersion.toVersion(version)
     tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = javaVersion.toString()
+        compilerOptions {
+            jvmTarget.set(JvmTarget.fromTarget(javaVersion.toString()))
         }
     }
 
