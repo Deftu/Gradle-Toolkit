4f4d55c76d824ade97c30e6454a96fd00e488181
Deftu <deftudev@gmail.com>
2024-06-27 17:38:37 +0200

    Add a `buildVersions` task to multiversion projects.

--- src/main/kotlin/dev/deftu/gradle/multiversion.gradle.kts
+++ src/main/kotlin/dev/deftu/gradle/multiversion.gradle.kts
@@ -3,6 +3,7 @@ package dev.deftu.gradle
 import com.replaymod.gradle.preprocess.PreprocessExtension
 import com.replaymod.gradle.preprocess.PreprocessPlugin
 import dev.deftu.gradle.utils.MCData
+import dev.deftu.gradle.utils.ModLoader
 import dev.deftu.gradle.utils.setupLoom
 
 plugins {
@@ -11,16 +12,34 @@ plugins {
 
 val mcData = MCData.from(project)
 
+// Set up `loom.platform` and apply Loom
 setupLoom(mcData)
-setupPreprocessor()
 
-fun setupPreprocessor() {
-    apply<PreprocessPlugin>()
-    extensions.configure<PreprocessExtension> {
-        vars.put("MC", mcData.version.rawVersion)
-        vars.put("FABRIC", if (mcData.isFabric) 1 else 0)
-        vars.put("FORGE-LIKE", if (mcData.isForge || mcData.isNeoForge) 1 else 0)
-        vars.put("FORGE", if (mcData.isForge) 1 else 0)
-        vars.put("NEOFORGE", if (mcData.isNeoForge) 1 else 0)
+// Set up preprocessor
+apply<PreprocessPlugin>()
+extensions.configure<PreprocessExtension> {
+    vars.put("MC", mcData.version.rawVersion)
+    vars.put("FABRIC", if (mcData.isFabric) 1 else 0)
+    vars.put("FORGE-LIKE", if (mcData.isForge || mcData.isNeoForge) 1 else 0)
+    vars.put("FORGE", if (mcData.isForge) 1 else 0)
+    vars.put("NEOFORGE", if (mcData.isNeoForge) 1 else 0)
+}
+
+// Define root tasks for ease-of-use
+val buildTaskName = "buildVersions"
+if (rootProject.tasks.none { task ->
+    task.name == buildTaskName
+}) {
+    rootProject.tasks.create(buildTaskName) {
+        group = "build"
+        description = "Build all versions"
+
+        rootProject.subprojects.map(Project::getName).filter { name ->
+            ModLoader.values().any { loader ->
+                name.endsWith("-${loader.name}", true)
+            }
+        }.forEach { version ->
+            dependsOn(":$version:build")
+        }
     }
 }
