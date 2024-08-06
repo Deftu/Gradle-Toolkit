92a10f15e3c5a31093a166c9046620c76dff438c
Deftu <me@deftu.dev>
2024-08-06 19:17:58 +0200

    Update Shadow from 7.1.2 to 8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("com.github.breadmoirai:github-release:2.5.2")
 
     // Other
-    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+    implementation("gradle.plugin.com.github.johnrengelman:shadow:8.1.1")
     implementation("dev.deftu:Bloom:0.1.2")
 }
 
