e8b244de71971869b59b7c3705cb7cbc629e68bc
Deftu <me@deftu.dev>
2024-09-04 13:55:39 +0200

    Make LoaderSpecificException more verbose

--- src/main/kotlin/dev/deftu/gradle/exceptions/LoaderSpecificException.kt
+++ src/main/kotlin/dev/deftu/gradle/exceptions/LoaderSpecificException.kt
@@ -2,4 +2,32 @@ package dev.deftu.gradle.exceptions
 
 import dev.deftu.gradle.utils.ModLoader
 
-class LoaderSpecificException(loader: ModLoader) : Exception("This resource can only be used with ${loader.name}.")
+class LoaderSpecificException(current: ModLoader? = null, vararg loaders: ModLoader) : Exception(generateMessage(current, loaders)) {
+
+    private companion object {
+
+        private fun generateMessage(current: ModLoader?, loaders: Array<out ModLoader>): String {
+            return if (loaders.size == 1) {
+                "This resource can only be used with ${loaders[0].name}."
+            } else {
+                val builder = StringBuilder("This resource can only be used with ")
+                for (i in loaders.indices) {
+                    builder.append(loaders[i].name)
+                    if (i < loaders.size - 2) {
+                        builder.append(", ")
+                    } else if (i == loaders.size - 2) {
+                        builder.append(" and ")
+                    }
+                }
+
+                builder.toString()
+            } + if (current != null) {
+                " (current: ${current.name})"
+            } else {
+                ""
+            }
+        }
+
+    }
+
+}
