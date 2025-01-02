12191f56193b2670b457372899caa064a581fbae
Deftu <me@deftu.dev>
2025-01-02 21:24:34 +0200

    i'm so tired, man

--- src/main/kotlin/dev/deftu/gradle/tools/ducks.gradle.kts
+++ src/main/kotlin/dev/deftu/gradle/tools/ducks.gradle.kts
@@ -1,5 +1,6 @@
 package dev.deftu.gradle.tools
 
+import gradle.kotlin.dsl.accessors._0935894d714bf6b98fac60b9fc45a2f5.jar
 import gradle.kotlin.dsl.accessors._0935894d714bf6b98fac60b9fc45a2f5.main
 import gradle.kotlin.dsl.accessors._0935894d714bf6b98fac60b9fc45a2f5.sourceSets
 import org.gradle.kotlin.dsl.java
@@ -9,9 +10,17 @@ plugins {
 }
 
 sourceSets {
-    val ducks by creating
+    val ducks by creating {
+        // Use the output of the `main` source set without modifying its classpath
+        compileClasspath += sourceSets["main"].compileClasspath
+    }
+
     main {
-        ducks.compileClasspath += compileClasspath
+        // Include the output of the `ducks` source set in the compile classpaths
         compileClasspath += ducks.output
     }
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
