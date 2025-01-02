b9e211f2eb42e986aa8e86c26c0a82641e731d0e
Deftu <me@deftu.dev>
2025-01-02 16:56:58 +0200

    Add ducks plugin

---
+++ src/main/kotlin/dev/deftu/gradle/tools/ducks.gradle.kts
@@ -0,0 +1,15 @@
+import gradle.kotlin.dsl.accessors._0935894d714bf6b98fac60b9fc45a2f5.main
+import gradle.kotlin.dsl.accessors._0935894d714bf6b98fac60b9fc45a2f5.sourceSets
+import org.gradle.kotlin.dsl.java
+
+plugins {
+    java
+}
+
+sourceSets {
+    val ducks by creating
+    main {
+        ducks.compileClasspath += compileClasspath
+        compileClasspath += ducks.output
+    }
+}
