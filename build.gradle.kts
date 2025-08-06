3b5d33cc794cd8e6bf173effe162c0a10d374b2b
Deftu <me@deftu.dev>
2025-08-06 15:50:58 +0200

    Bump Preprocessor

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("dev.architectury:architectury-pack200:0.1.3")
 
     // Preprocessing/multi-versioning
-    implementation("dev.deftu:preprocessor:0.14.0")
+    implementation("dev.deftu:preprocessor:0.15.0")
 
     // Documentation
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.0.0")
