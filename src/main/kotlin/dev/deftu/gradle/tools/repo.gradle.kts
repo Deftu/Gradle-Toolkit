e3aa24004546d73e3ba76c92407411498372887a
Deftu <me@deftu.dev>
2025-12-14 13:05:06 +0200

    More repositories added to auto-repo

--- src/main/kotlin/dev/deftu/gradle/tools/repo.gradle.kts
+++ src/main/kotlin/dev/deftu/gradle/tools/repo.gradle.kts
@@ -12,20 +12,14 @@ repositories {
     optionalMaven("repo.deftu.snapshots", "https://maven.deftu.dev/snapshots/", "Deftu Snapshots")
 
     // Minecraft
+    optionalMaven("repo.modrinth", "https://api.modrinth.com/maven/", "Modrinth") { onlyForGroups("maven.modrinth") }
     optionalMaven("repo.essential", "https://repo.essential.gg/repository/maven-public/", "Essential")
     optionalMaven("repo.kff", "https://thedarkcolour.github.io/KotlinForForge/", "KotlinForForge")
-
-    optionalMaven("repo.deftu.mirror", "https://maven.deftu.dev/mirror", "Deftu Mirror") {
-        content {
-            includeGroup("com.terraformersmc") // Mod Menu
-        }
-    }
-
-    optionalMaven("repo.sponge", "https://repo.spongepowered.org/maven/", "SpongePowered") {
-        content {
-            includeGroup("org.spongepowered")
-        }
-    }
+    optionalMaven("repo.teamresourceful", "https://maven.teamresourceful.com/repository/maven-releases", "Team Resourceful Releases")
+    optionalMaven("repo.bawnorton", "https://maven.bawnorton.com/releases", "Bawnorton Releases") { onlyForGroups("com.github.bawnorton") }
+    optionalMaven("repo.neu", "https://maven.notenoughupdates.org/releases", "NotEnoughUpdates Releases") { onlyForGroups("org.notenoughupdates.moulconfig") }
+    optionalMaven("repo.deftu.mirror", "https://maven.deftu.dev/mirror", "Deftu Mirror") { onlyForGroups("com.terraformersmc") }
+    optionalMaven("repo.sponge", "https://repo.spongepowered.org/maven/", "SpongePowered") { onlyForGroups("org.spongepowered") }
 }
 
 fun RepositoryHandler.optionalMaven(
@@ -44,5 +38,13 @@ fun RepositoryHandler.optionalMaven(
         }
 
         block()
+    }
+}
+
+fun ArtifactRepository.onlyForGroups(vararg groups: String) {
+    content {
+        for (group in groups) {
+            includeGroup(group)
+        }
     }
 }
